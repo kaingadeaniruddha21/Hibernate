4a68b73572fd32ac8b4ef4b291324d0d1370ed6d
kaingadeaniruddha21 <93026301+kaingadeaniruddha21@users.noreply.github.com>
2022-01-28 17:32:47 +0530

    Add files via upload

---
+++ ManyToManyExample/src/main/java/com/training/manytomany/dao/ItemsDao.java
@@ -0,0 +1,56 @@
+package com.training.manytomany.dao;
+
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.training.manytomany.entity.Items;
+import com.training.manytomany.util.HibernateUtil;
+
+public class ItemsDao {
+	public void saveItems(Items items) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			
+			// start the transaction
+			transaction = session.beginTransaction();
+			// save the employee object in to database
+			session.save(items);
+			// commit transaction
+			transaction.commit();
+		}catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+
+			e.printStackTrace();
+		}
+	}
+
+	public Items getItems(Long itemid) {
+		Transaction transaction = null;
+		Items projectById = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			
+			// start the transaction
+			transaction = session.beginTransaction();
+			// save the employee object in to database
+			//session.saveOrUpdate(emp);
+			
+			//get entity from database
+			projectById = session.get(Items.class, itemid);
+			if(projectById!=null) {
+				
+				System.out.println("Employee details are = " + projectById);
+			}
+		}catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+
+			e.printStackTrace();
+		}
+		return projectById;
+		
+	}
+}
---
+++ ManyToManyExample/src/main/java/com/training/manytomany/util/HibernateUtil.java
@@ -0,0 +1,52 @@
+package com.training.manytomany.util;
+
+
+
+import java.util.Properties;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import com.training.manytomany.entity.Orders;
+
+import com.training.manytomany.entity.Items;
+
+public class HibernateUtil {
+	public static SessionFactory sessionFactory;
+	public static SessionFactory getSessionFactory() {
+		if(sessionFactory == null) {
+			try {
+				Configuration cfg = new Configuration();
+				Properties settings = new Properties();
+				settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/example_manytomany_data");
+				settings.put(Environment.USER,"root");
+				settings.put(Environment.PASS,"root");
+				settings.put(Environment.DIALECT,"org.hibernate.dialect.MySQL5Dialect");
+				settings.put(Environment.SHOW_SQL,"true");
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS,"thread");
+				settings.put(Environment.HBM2DDL_AUTO,"create");
+				
+				cfg.setProperties(settings);
+				cfg.addAnnotatedClass(Orders.class);
+				cfg.addAnnotatedClass(Items.class);
+				
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(cfg.getProperties()).build();
+				System.out.println("Hibernate java configuration service registry created...");
+				sessionFactory = cfg.buildSessionFactory(serviceRegistry);
+			
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sessionFactory;
+
+		
+	}
+	
+	
+}
